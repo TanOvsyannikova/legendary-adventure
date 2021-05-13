b0c512a20b9e24ce64f3cb30b2138e5054ed76db
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-05-13 22:02:00 +0300

    Add a custom view

--- MemoGame/MemoGame/EmojiMemoGameView.swift
+++ MemoGame/MemoGame/EmojiMemoGameView.swift
@@ -40,6 +40,7 @@ struct CardView: View {
                 if card.isFaceUp {
                     RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                     RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(120-90)).padding(5).opacity(0.4)
                     Text(card.content)
                 } else {
                     if !card.isMatched{
@@ -56,14 +57,16 @@ struct CardView: View {
     private let edgeLineWidth: CGFloat = 3
     
     private func fontSize (for size: CGSize) -> CGFloat {
-        min(size.width , size.height) * 0.75
+        min(size.width , size.height) * 0.7
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoGameView(viewModel: EmojiMemoGame())
+        let game = EmojiMemoGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoGameView(viewModel: game)
     }
     
 }
---
+++ MemoGame/MemoGame/Pie.swift
@@ -0,0 +1,29 @@
+//
+//  Pie.swift
+//  MemoGame
+//
+//  Created by Татьяна Овсянникова on 13.05.2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = true
+    
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(x: center.x + radius * cos(CGFloat(startAngle.radians)), y: center.y + radius * sin(CGFloat(startAngle.radians)))
+                
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        p.addLine(to: center)
+        
+        return p
+    }
+}
